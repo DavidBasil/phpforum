3b91d93c49ede43ad471560cd8b554a3f93fe9a9
David Basil <info@basildavid.com>
2019-06-02 19:57:11 +0400

    add footer.php

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+	</div> <!-- /starter-template -->
+</div> <!-- /container -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js" charset="utf-8"></script>
